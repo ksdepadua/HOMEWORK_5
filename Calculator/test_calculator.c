4eaa167c211d8bc2b772c7bf0ae88886b94fea97
Keana De Padua <ksdepadua@cpp.edu>
2026-02-26 17:52:38 -0800

    Modified test_calculator.c, test_runner.exe; Added 3 more passing tests

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -16,12 +16,28 @@ void tearDown(void) {
 }
 
 void test_add_positive_numbers(void) {
-    TEST_ASSERT_EQUAL(5, add(2, 3)); // We expect 2 + 3 = 5
-                                      // PASS if true, FAIL if false
+    TEST_ASSERT_EQUAL(5, add(2, 3));    // We expect 2 + 3 = 5
+}
+
+void test_add_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(1, add(5, -4));   // Expect 5 + -4 = 1
+}
+
+void test_add_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-7, add(-3, -4)); // Expect -3 + -4 = -7
+}
+
+// Can do multiple validations in one test
+void test_add_zero(void) {
+    TEST_ASSERT_EQUAL(10, add(10, 0));  // Expect 10 + 0 = 10
+    TEST_ASSERT_EQUAL(0, add(0, 0));    // Expect 0 + 0 = 0
 }
 
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_add_positive_numbers); // Run our test function
+    RUN_TEST(test_add_positive_numbers); // Run our test function(s)
+    RUN_TEST(test_add_positive_and_negative_numbers);
+    RUN_TEST(test_add_negative_numbers);
+    RUN_TEST(test_add_zero);
     return UNITY_END();
 }

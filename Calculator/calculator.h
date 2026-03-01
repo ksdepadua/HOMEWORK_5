e64dd6293735d090d8ec2d0ed85fcd052e98b824
Keana De Padua <ksdepadua@cpp.edu>
2026-02-28 17:37:27 -0800

    Created stub function for subtract() in calculator.c and calculator.h; Created subtract() tests in test_calculator.c; All tests failed

--- Calculator/calculator.h
+++ Calculator/calculator.h
@@ -2,5 +2,6 @@
 #define CALCULATOR_H    //  define CALCULATOR_H
 
 int add(int a, int b);  // Allocated signed int type (32 bits/4 bytes)
+int subtract(int a, int b);
 
 #endif                  // end if
--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -15,6 +15,7 @@ void tearDown(void) {
     // In this example, we don't need anything here.
 }
 
+/* ADD() TESTS */
 void test_add_positive_numbers(void) {
     TEST_ASSERT_EQUAL(5, add(2, 3));    // We expect 2 + 3 = 5
 }
@@ -43,13 +44,40 @@ void test_add_underflow(void) {
     TEST_ASSERT_TRUE(result > 0);       // Checks if underflow occurred
 }
 
+/* SUBTRACT() TESTS */
+void test_subtract_positive_numbers(void) {
+    TEST_ASSERT_EQUAL(3, subtract(8, 5));       // Expect 8 - 5 = 3
+}
+
+void test_subtract_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(9, subtract(6, -3));      // Expect 6 - -3 = 9
+}
+
+void test_subtract_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(1, subtract(-3, -4));     // Expect -3 - -4 = 1
+}
+
+void test_subtract_zero(void) {
+    TEST_ASSERT_EQUAL(10, subtract(10, 0));     // Expect 10 - 0 = 10
+    TEST_ASSERT_EQUAL(0, subtract(0, 0));       // Expect 0 - 0 = 0
+}
+
 int main(void) {
     UNITY_BEGIN();
+
+    /* ADD() TESTS */
     RUN_TEST(test_add_positive_numbers); // Run our test function(s)
     RUN_TEST(test_add_positive_and_negative_numbers);
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
     RUN_TEST(test_add_overflow);
     RUN_TEST(test_add_underflow);
+
+    /* SUBTRACT() TESTS */
+    RUN_TEST(test_subtract_positive_numbers);
+    RUN_TEST(test_subtract_positive_and_negative_numbers);
+    RUN_TEST(test_subtract_negative_numbers);
+    RUN_TEST(test_subtract_zero);
+    
     return UNITY_END();
 }

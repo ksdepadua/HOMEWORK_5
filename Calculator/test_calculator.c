61b038ab2df3318a14d944ffba00316847f9e16a
Keana De Padua <ksdepadua@cpp.edu>
2026-02-28 17:42:43 -0800

    Created overflow and underflow tests for subtract()

--- Calculator/test_calculator.c
+++ Calculator/test_calculator.c
@@ -62,6 +62,16 @@ void test_subtract_zero(void) {
     TEST_ASSERT_EQUAL(0, subtract(0, 0));       // Expect 0 - 0 = 0
 }
 
+void test_subtract_overflow(void) {
+    int result = subtract(INT_MAX, -1);         // INT_MAX - -1 should wrap around to the "bottom"
+    TEST_ASSERT_TRUE(result < 0);
+}
+
+void test_subtract_underflow(void) {
+    int result = subtract(INT_MIN, 1);          // INT_MIN - 1 should wrap around to the "top"
+    TEST_ASSERT_TRUE(result > 0);
+}
+
 int main(void) {
     UNITY_BEGIN();
 
@@ -78,6 +88,8 @@ int main(void) {
     RUN_TEST(test_subtract_positive_and_negative_numbers);
     RUN_TEST(test_subtract_negative_numbers);
     RUN_TEST(test_subtract_zero);
+    RUN_TEST(test_subtract_overflow);
+    RUN_TEST(test_subtract_underflow);
     
     return UNITY_END();
 }
